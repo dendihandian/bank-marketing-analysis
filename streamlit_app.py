a1d38b54e59fd8bf4b73b6215095ecab98c5d2e1
Dendi Handian <contact.dendi@yahoo.com>
2022-03-26 05:10:05 +0700

    decisiontree and logisticregression dumps

---
+++ streamlit_app.py
@@ -0,0 +1,60 @@
+from utilities.helper import categoricals_map, numericals_minmax, model_names, predict
+import streamlit as st
+
+###### Variables
+job = None
+marital = None
+education = None
+previous = None
+salary = None
+balance = None
+duration = None
+pdays = None
+response = None
+
+##### The App
+
+st.title('Bank Marketing Analysis and Modeling', anchor=None)
+
+st.write("""
+    - [Exploratory Data Analysis](https://github.com/dendihandian/bank-marketing-analysis/blob/main/bank-marketing.ipynb)
+    - [Dataset](https://www.kaggle.com/datasets/dhirajnirne/bank-marketing)
+""")
+
+with st.container():
+
+    st.header('Response Predictor')
+
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        job = st.selectbox('job'.title(), categoricals_map['job'].keys())
+        marital = st.selectbox('marital'.title(), categoricals_map['marital'].keys())
+        education = st.selectbox('education'.title(), categoricals_map['education'].keys())
+        previous = st.selectbox('previous'.title(), categoricals_map['previous'].keys())
+
+    with col2:
+        salary = st.slider('salary'.title(), min_value=numericals_minmax['salary'][0], max_value=numericals_minmax['salary'][1])
+        balance = st.slider('balance'.title(), min_value=numericals_minmax['balance'][0], max_value=numericals_minmax['balance'][1])
+        duration = st.slider('duration'.title(), min_value=numericals_minmax['duration'][0], max_value=numericals_minmax['duration'][1])
+        pdays = st.slider('pdays'.title(), min_value=numericals_minmax['pdays'][0], max_value=numericals_minmax['pdays'][1])
+
+    modelname = st.selectbox('Model', model_names)
+
+    if st.button('Predict'):
+        response = predict({
+            'job': job,
+            'marital': marital,
+            'education': education,
+            'previous': previous,
+            'salary': salary,
+            'balance': balance,
+            'duration': duration,
+            'pdays': pdays,
+        }, modelname)
+
+    if response != None:
+        st.metric('Response', response, delta=None, delta_color="normal")
+
+    # st.json(numericals_minmax)
---
+++ utilities/helper.py
@@ -0,0 +1,61 @@
+import pickle
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+numericals_minmax = pickle.load(open('serialization/utilities/numericals_minmax.pickle', 'rb')) # NOTE: you can debug the min_max using st.json(min_max)
+categoricals_map = pickle.load(open('serialization/utilities/categoricals_map.pickle', 'rb')) # NOTE: you can debug the min_max using st.json(min_max)
+scaler_salary = pickle.load(open('serialization/utilities/scaler_salary.pickle', 'rb'))
+scaler_balance = pickle.load(open('serialization/utilities/scaler_balance.pickle', 'rb'))
+scaler_duration = pickle.load(open('serialization/utilities/scaler_duration.pickle', 'rb'))
+scaler_pdays = pickle.load(open('serialization/utilities/scaler_pdays.pickle', 'rb'))
+
+model_names = (
+    'DecisionTreeClassifier',
+    'LogisticRegression',
+)
+
+decision_tree_classifier_tuned = pickle.load(open('serialization/models/decision_tree_classifier_tuned.pickle', 'rb'))
+logistic_regression_tuned = pickle.load(open('serialization/models/logistic_regression_tuned.pickle', 'rb'))
+
+def predict(parameters, modelname):
+
+    job = parameters['job']
+    marital = parameters['marital']
+    education = parameters['education']
+    marital_education = marital + "-" + education
+    previous = parameters['previous']
+    salary = parameters['salary']
+    balance = parameters['balance']
+    duration = parameters['duration']
+    pdays = parameters['pdays']
+
+    inputs = pd.DataFrame({
+        'salary': [scaler_salary.transform(np.array([salary]).reshape(1, -1))[0][0]],
+        'balance': [scaler_balance.transform(np.array([balance]).reshape(1, -1))[0][0]],
+        'duration': [scaler_duration.transform(np.array([duration]).reshape(1, -1))[0][0]],
+        'pdays': [scaler_pdays.transform(np.array([pdays]).reshape(1, -1))[0][0]],
+
+        'job': [categoricals_map['job'][job]],
+        'marital': [categoricals_map['marital'][marital]],
+        'education': [categoricals_map['education'][education]],
+        'marital-education': [categoricals_map['marital-education'][marital_education]],
+        'previous': [categoricals_map['previous'][previous]],
+    })
+
+    output = None
+    response = None
+
+    if (modelname == model_names[0]):
+
+        output = decision_tree_classifier_tuned.predict(inputs)
+
+    elif(modelname == model_names[1]):
+
+        output = logistic_regression_tuned.predict(inputs)
+
+    # if (output):
+    response_decoder = {0: 'No', 1: 'Yes'}
+    response = response_decoder[output[0]]
+
+    return response
